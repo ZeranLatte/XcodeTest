701abca69dc812a22eec4e122571dc7a72cc971a
zeran <zeranwang24@gmail.com>
2017-12-27 23:56:56 -0800

    commit

---
+++ XCTestTemplate/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  XCTestTemplate
+//
+//  Created by ZZZZeran on 12/27/17.
+//  Copyright © 2017 LatteZW. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
